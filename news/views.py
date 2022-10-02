a01cda4b7404b1c99e7104edbc8bc2597569ce0c
Valera Medokvin <v.medovkin1664@gmail.com>
2022-10-03 00:06:27 +0300

    initial commit

---
+++ news/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
+
+from .models import News, Category
+from .forms import NewsForm
+
+
+class HomeNews(ListView):
+    model = News
+    template_name = 'news/news_list.html'
+    context_object_name = 'news'
+    # extra_context = {'title': 'Главная'}
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Главная страница'
+        return context
+
+    def get_queryset(self):
+        return News.objects.filter(is_published=True).select_related('category')
+
+
+class NewsByCategory(ListView):
+    model = News
+    template_name = 'news/news_list.html'
+    context_object_name = 'news'
+    allow_empty = False
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = Category.objects.get(pk=self.kwargs['category_id'])
+        return context
+
+    def get_queryset(self):
+        return News.objects.filter(category_id=self.kwargs['category_id'], is_published=True).select_related('category')
+
+
+class ViewNews(DetailView):
+    model = News
+    context_object_name = 'news_item'
+    template_name = 'news/news_detail.html'
+    # pk_url_kwarg = 'news_id'
+
+
+class CreateNews(CreateView):
+    form_class = NewsForm
+    template_name = 'news/add_news.html'
+    # success_url = reverse_lazy('home')
+
