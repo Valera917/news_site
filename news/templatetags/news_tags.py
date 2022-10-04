3e10a5533666b394fb73e79ba01fa83e8746bd9a
Valera Medokvin <v.medovkin1664@gmail.com>
2022-10-04 12:49:56 +0300

    fix sidebar

--- news/templatetags/news_tags.py
+++ news/templatetags/news_tags.py
@@ -12,6 +12,6 @@ def get_categories():
 
 
 @register.inclusion_tag('news/list_categories.html')
-def show_categories(arg1='Hello', arg2='world'):
+def show_categories():
     categories = Category.objects.annotate(cnt=Count('news', filter=F('news__is_published'))).filter(cnt__gt=0)
-    return {"categories": categories, "arg1": arg1, "arg2": arg2}
+    return {"categories": categories}
