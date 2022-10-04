196b5c6535a97f89f137a1e550b123391d134707
Valera Medokvin <v.medovkin1664@gmail.com>
2022-10-04 22:25:27 +0300

    add mixin and fix list categories

--- news/views.py
+++ news/views.py
@@ -7,17 +7,18 @@ from django.contrib.auth import login
 
 from .models import News, Category
 from .forms import NewsForm, UserRegisterForm, UserLoginForm
+from .utils import DataMixin
 
 
-class RegisterUser(CreateView):
+class RegisterUser(DataMixin, CreateView):
     form_class = UserRegisterForm
     template_name = 'news/register.html'
     redirect_authenticated_user = True
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Registration'
-        return context
+        c_def = self.get_user_context(title='Registration')
+        return dict(list(context.items()) + list(c_def.items()))
 
     def form_valid(self, form):
         user = form.save()
@@ -25,65 +26,64 @@ class RegisterUser(CreateView):
         return redirect('home')
 
 
-class LoginUser(LoginView):
+class LoginUser(DataMixin, LoginView):
     form_class = UserLoginForm
     template_name = 'news/login.html'
     redirect_authenticated_user = True
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Authorization'
-        return context
+        c_def = self.get_user_context(title='Authorization')
+        return dict(list(context.items()) + list(c_def.items()))
 
     def get_success_url(self):
         return reverse_lazy('home')
 
 
-class HomeNews(ListView):
+class HomeNews(DataMixin, ListView):
     model = News
     template_name = 'news/news_list.html'
     context_object_name = 'news'
-    paginate_by = 2
-    # extra_context = {'title': 'Главная'}
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Главная страница'
-        return context
+        c_def = self.get_user_context(title='Главная страница')
+        # context['title'] = 'Главная страница'
+        return dict(list(context.items()) + list(c_def.items()))
 
     def get_queryset(self):
         return News.objects.filter(is_published=True).select_related('category')
 
 
-class NewsByCategory(ListView):
+class NewsByCategory(DataMixin, ListView):
     model = News
     template_name = 'news/news_list.html'
     context_object_name = 'news'
     allow_empty = False
-    paginate_by = 2
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = Category.objects.get(pk=self.kwargs['category_id'])
-        return context
+        cat = Category.objects.get(pk=self.kwargs['category_id'])
+        c_def = self.get_user_context(title=f'Category - {str(cat.title)}',)
+        return dict(list(context.items()) + list(c_def.items()))
 
     def get_queryset(self):
         return News.objects.filter(category_id=self.kwargs['category_id'], is_published=True).select_related('category')
 
 
-class ViewNews(DetailView):
+class ViewNews(DataMixin, DetailView):
     model = News
     context_object_name = 'news_item'
     template_name = 'news/news_detail.html'
     # pk_url_kwarg = 'news_id'
 
 
-class CreateNews(LoginRequiredMixin, CreateView):
+class CreateNews(LoginRequiredMixin, DataMixin, CreateView):
     form_class = NewsForm
     template_name = 'news/add_news.html'
     # success_url = reverse_lazy('home')
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Добавление новости'
-        return context
+        c_def = self.get_user_context(title='Добавление новости')
+        return dict(list(context.items()) + list(c_def.items()))
